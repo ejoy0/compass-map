33826e6311d032334993fb8ea56ddd6b35316a05
ejoy <112738485+ejoygithub@users.noreply.github.com>
2023-04-09 23:02:29 +0900

    Delete main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-const debug = document.getElementById("debug");
-
-window.addEventListener("deviceorientation", (event) => {
-    console.log(`${event.alpha} : ${event.beta} : ${event.gamma}`);
-    debug.textContent = event.alpha, event.beta, event.gamma
-});
