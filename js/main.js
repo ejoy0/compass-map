5df7d7e3bd0d7034998f308897fcff5eaadc6fc3
ejoy github <ogurei468@gmail.com>
2023-04-17 20:36:48 +0900

    use geolocation property

--- js/main.js
+++ js/main.js
@@ -3,8 +3,30 @@
 {
     const debug = document.getElementById("debug");
     
-    window.addEventListener("deviceorientation", (e) => {
-        debug.textContent = e.beta;
+    window.addEventListener("GeolocationCoordinates", (e) => {
+        debug.textContent = `${e.latitude}, {e.longtitude}`;
         console.log(e.beta);
     });
+
+    // const canvas = document.getElementById("canvas")
+
+    // document.querySelector('input[type="file"]').onchange = function() {
+    //     let img = this.files[0]
+    //     let reader = new FileReader()
+    //     reader.readAsDataURL(img)
+    //     reader.onload = function() {
+    //         drawImage(reader.result)
+    //     }
+    // }
+
+    // function drawImage(url) {
+    //     let ctx = canvas.getContext('2d')
+    //     let image = new Image()
+    //     image.src = url
+    //     image.onload = () => {
+    //         canvas.width = image.width
+    //         canvas.height = image.height
+    //         ctx.drawImage(image, 0, 0)
+    //     }
+    // }
 }
